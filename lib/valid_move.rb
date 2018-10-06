967ec1e4c0b3848a41afafce6ca0cc5816bd0859
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-06 23:58:09 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,10 @@
- def valid_move? (board, oneBasedIndex)
-   return (oneBasedIndex - 1).between?(0, board.size)
+ def valid_move? (board, index)
+   return index.between?(0, board.size) && !position_taken?(board, index)
  end
 
+def position_taken?(board, index)
+  spot = board[index]
+  return !(spot == " " || spot == "" || !spot)
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
